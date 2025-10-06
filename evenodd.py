d203420f63d51306263ea74bfdce1319b3b6dc9d
shrushtir2006 <shrushtiryavanki@gmail.com>
2025-10-06 17:10:45 +0530

    Update evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -18,3 +18,27 @@ def count_even_odd(numbers):
 numbers = [2, 3, 4, 5, 6, 7, 9, 11, 13, 14]
 
 count_even_odd(numbers)
+# Prime Number Counter Program
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def count_primes(numbers):
+    prime_count = 0
+
+    for n in numbers:
+        if is_prime(n):
+            prime_count += 1
+
+    print("Prime numbers:", prime_count)
+
+
+# Example input
+numbers = [2, 3, 4, 5, 6, 7, 9, 11, 13, 14]
+
+count_primes(numbers)
